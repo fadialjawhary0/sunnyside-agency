91347905922260b4a6214319587f38f7bd5e107f
fadialjohari0 <fadialjohari0@gmail.com>
2023-12-09 01:38:34 +0300

    cleanups

--- src/App.jsx
+++ src/App.jsx
@@ -5,48 +5,48 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 import Layout from './layout';
 
-// const theme = createTheme({
-//   typography: {
-//     fontFamily: ['"Barlow Semi Condensed"', 'Arial', 'sans-serif'].join(','),
-//   },
-//   components: {
-//     MuiButton: {
-//       styleOverrides: {
-//         root: {
-//           color: '#ffffff',
-//           borderColor: '#ffffff',
-//           '&:hover': {
-//             borderColor: '#c9c9c9',
-//           },
-//         },
-//         contained: {
-//           backgroundColor: '#e4e4e4',
-//           color: '#3b4363',
-//           '&:hover': {
-//             backgroundColor: '#f3f3f3',
-//             color: '#BA2D4A',
-//           },
-//         },
-//       },
-//     },
+const theme = createTheme({
+  typography: {
+    fontFamily: ['"Barlow Semi Condensed"', 'Arial', 'sans-serif'].join(','),
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          color: '#ffffff',
+          borderColor: '#ffffff',
+          '&:hover': {
+            borderColor: '#c9c9c9',
+          },
+        },
+        contained: {
+          backgroundColor: '#e4e4e4',
+          color: '#3b4363',
+          '&:hover': {
+            backgroundColor: '#f3f3f3',
+            color: '#BA2D4A',
+          },
+        },
+      },
+    },
 
-//     MuiDialogTitle: {
-//       styleOverrides: {
-//         root: {
-//           padding: 0,
-//           lineHeight: 1,
-//         },
-//       },
-//     },
-//     MuiTypography: {
-//       styleOverrides: {
-//         root: {
-//           color: '#ffffff',
-//         },
-//       },
-//     },
-//   },
-// });
+    MuiDialogTitle: {
+      styleOverrides: {
+        root: {
+          padding: 0,
+          lineHeight: 1,
+        },
+      },
+    },
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          color: '#ffffff',
+        },
+      },
+    },
+  },
+});
 
 export const App = () => {
   return (
--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -7,12 +7,6 @@ const Layout = () => {
     <Box
       sx={{
         height: '100vh',
-        padding: '2rem',
-        background: 'radial-gradient(hsl(214, 47%, 23%), hsl(237, 49%, 15%))',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        gap: '5rem',
       }}></Box>
   );
 };
